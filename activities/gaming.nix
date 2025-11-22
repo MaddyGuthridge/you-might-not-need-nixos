17fe377c4e53f98a34d78da4e8073f836b7b7c10
Maddy Guthridge <hello@maddyguthridge.com>
2025-11-22 23:21:07 +1100

    Use provided hardware config for Dell XPS 9500

--- activities/gaming.nix
+++ activities/gaming.nix
@@ -3,8 +3,18 @@
   # Steam
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    # Open ports in the firewall for Steam Remote Play
+    remotePlay.openFirewall = true;
+    # Open ports in the firewall for Source Dedicated Server
+    dedicatedServer.openFirewall = true;
+    # Open ports in the firewall for Steam Local Network Game Transfers
+    localNetworkGameTransfers.openFirewall = true;
   };
+  # Enable gamescope session as alternate login
+  programs.steam.gamescopeSession.enable = true;
+  programs.gamescope.enable = true;
+  # Mangohud shows FPS
+  environment.systemPackages = with pkgs; [
+    mangohud
+  ];
 }
--- hardware/default.nix
+++ hardware/default.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 {
   imports = [
-    ./nvidia.nix
+    # Wait there is a pre-made hardware configuration specifically for my system
+    # which will automatically set up my GPU and a bunch of other things?!
+    "${
+      builtins.fetchGit { url = "https://github.com/NixOS/nixos-hardware.git"; }
+    }/dell/xps/15-9500/nvidia"
   ];
 }
